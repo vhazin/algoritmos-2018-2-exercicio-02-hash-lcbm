b8d3820d77071d97718b960d1757198d448d8980
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-08-21 07:13:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
-int main(){
-    unsigned int t, line = 0;
-    scanf("%u", &t);
-
+int main(void){
+    unsigned long int t, line = 0;
+    scanf("%lu", &t);
     unsigned long int output[t], counter;
     while (t > line){
         unsigned long int a, b, x, n, c, d, m;
         scanf("%lu %lu %lu %lu %lu %lu %lu", &a, &b, &x, &n, &c, &d, &m);
-
         for (unsigned int index = 0; index <= n; index++){
             unsigned long int hx = a * (x + index) + b % m;
             if (c <= hx <= d && hx <= m - 1){
                 counter++;
             };
         };
-
         output[line] = counter;
         counter = 0;
         line++;
     };
-
     for (line = 0; line < t; line++){
-        printf("%lu\n", output[line]);
+        printf("%lu", output[line]);
+        printf("\n");
     };
-    
     return 0;
 }
