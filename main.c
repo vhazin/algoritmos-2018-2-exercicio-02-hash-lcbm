d2473bbca4e50f3609fd986b90ab086b4b76ed0a
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-08-23 13:22:52 -0300

    Time limit

--- main.c
+++ main.c
@@ -3,22 +3,18 @@
 int main(void){
     unsigned int t;
     scanf("%u", &t);
-    unsigned long long output[t];
+
     while (t--){
         unsigned long long a, b, x, n, c, d, m, counter = 0;
-        scanf("%llu %llu %llu %llu %llu %llu %llu", &a, &b, &x, &n, &c, &d, &m);
+        scanf("%llu %llu %llu %llu %llu %llu %llu", &a, &b, &x, &n, &c, &d, &m);     
         for (unsigned long long index = 0; index <= n; index++){
-            unsigned long long hx = a * (x + index) + b % m;
-            if ((c <= hx <= d) && (hx <= (m - 1))){
+            unsigned long long hx = (a * (x + index) + b) % m;
+            if ((c <= hx) && (hx <= d) && (hx <= (m - 1))){
                 counter++;
-            };
+                };
         };
-        output[t] = counter;
+        printf("%llu\n", counter);
         counter = 0;
-        // printf("%llu\n", counter);
-    };
-    for (t = 0; t < sizeof(output)/sizeof(unsigned long long); t++){
-        printf("%llu\n", output[t]);
     };
     return 0;
-};
+}
